5e7a3575227650a30ed307e573b93c860fd5dd37
chenjian07 <chenjian07@meituan.com>
2026-04-16 17:27:28 +0800

    fix: disable sharp image service to resolve CI build failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://markcj.cc',
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop',
+    },
+  },
   markdown: {
     remarkPlugins: [],
     rehypePlugins: [],
